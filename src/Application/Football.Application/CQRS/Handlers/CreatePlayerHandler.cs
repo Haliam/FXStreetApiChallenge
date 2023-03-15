2ba57d7bda35e49ce19f6e451d66bd4b21399818
Haliam <haliampf@gmail.com>
2023-03-15 21:17:10 +0000

    Add Handlers

---
+++ src/Application/Football.Application/CQRS/Handlers/CreatePlayerHandler.cs
@@ -0,0 +1,24 @@
+﻿using Football.Application.CQRS.Commands;
+using Football.Application.CQRS.Responses;
+using Football.Application.Services;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Football.Application.CQRS.Handlers
+{
+    public class CreatePlayerHandler : IRequestHandler<CreatePlayerCommand, CreatePlayerResponse>
+    {
+        private IPlayerCommandService _playerCommandService;
+
+        public CreatePlayerHandler(IPlayerCommandService playerCommandService)
+        {
+            _playerCommandService = playerCommandService;
+        }
+
+        public async Task<CreatePlayerResponse> Handle(CreatePlayerCommand request, CancellationToken cancellationToken)
+        {
+            return await _playerCommandService.Insert(request, cancellationToken);         
+        }
+    }
+}
---
+++ src/Application/Football.Application/CQRS/Handlers/GetPlayerByIdHandler.cs
@@ -0,0 +1,25 @@
+﻿using Football.Application.CQRS.Queries;
+using Football.Application.CQRS.Responses;
+using Football.Application.Services;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Football.Application.CQRS.Handlers
+{
+    public class GetPlayerByIdHandler : IRequestHandler<GetPlayerByIdQuery, GetPlayerByIdResponse>
+    {
+        private IPlayerQueryService _playerQueryService;
+
+        public GetPlayerByIdHandler(IPlayerQueryService playerQueryService)
+        {
+            _playerQueryService = playerQueryService;
+        }
+
+        public async Task<GetPlayerByIdResponse> Handle(GetPlayerByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _playerQueryService.GetById(request.Id, cancellationToken);
+        }
+    }
+}
