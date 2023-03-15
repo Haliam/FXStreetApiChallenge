a0b92ad2d3fa6efd4cfd97c4d707671d24df2956
Haliam <haliampf@gmail.com>
2023-03-15 21:20:08 +0000

    Add PlayerProfile mapping

---
+++ src/Application/Football.Application/Mapping/PlayerProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Football.Application.CQRS.Commands;
+using Football.Application.CQRS.Queries;
+using Football.Application.CQRS.Responses;
+using Football.Domain.Models;
+
+namespace Football.Application.Mapping
+{
+    public class PlayerProfile : Profile
+    {
+        public PlayerProfile()
+        {
+            CreateMap<Player, CreatePlayerCommand>().ReverseMap();
+            CreateMap<Player, CreatePlayerResponse>().ReverseMap();
+
+            CreateMap<Player, GetPlayerByIdQuery>().ReverseMap();
+            CreateMap<Player, GetPlayerByIdResponse>().ReverseMap();
+        }
+    }
+}
