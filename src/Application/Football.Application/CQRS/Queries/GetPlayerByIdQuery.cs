f3b656f464d757c365d117c1f58a331067d17a28
Haliam <haliampf@gmail.com>
2023-03-15 21:18:01 +0000

    Add queries

---
+++ src/Application/Football.Application/CQRS/Queries/GetPlayerByIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using Football.Application.CQRS.Responses;
+using Football.Domain.Models;
+using MediatR;
+
+namespace Football.Application.CQRS.Queries
+{
+    public class GetPlayerByIdQuery : IRequest<GetPlayerByIdResponse>
+    {
+        public int Id { get; set; }
+
+        public GetPlayerByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ src/Application/Football.Application/CQRS/Queries/GetPlayersQuery.cs
@@ -0,0 +1,12 @@
+﻿using Football.Application.CQRS.Responses;
+using Football.Domain.Models;
+using MediatR;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Football.Application.CQRS.Queries
+{
+    public class GetPlayersQuery : IRequest<GetPlayersResponse>
+    {
+    }
+}
